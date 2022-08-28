78695f13203bc98145c64f6cfe23cf22cc148a8e
John Vanek <62773153+JohnVanek@users.noreply.github.com>
2022-08-27 23:06:57 -0400

    fix layout and toolbar

--- app/src/main/java/com/example/android/mygreatuniversity/MainActivity.java
+++ app/src/main/java/com/example/android/mygreatuniversity/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.android.mygreatuniversity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //sets the toolbar defined in the layout as the action bar
+        //Which enables me to use more methods.
+        Toolbar myToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
+        //This
+        //getSupportActionBar().setHomeAsUpIndicator();//This changes the logo for the up indicator
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);   //show back button
+    }
+
+
+    //Inflate the menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.overflow_menu, menu);
+        return true;
+    }
+
+    public void gotoTermView(MenuItem item) {
+        System.out.println("hello");
+        Intent intent = new Intent(MainActivity.this, TermView.class);
+        startActivity(intent);
     }
 }

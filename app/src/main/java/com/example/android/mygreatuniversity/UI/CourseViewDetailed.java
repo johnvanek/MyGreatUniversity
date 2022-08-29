57547f7cf1e410721494d0b0e61f6546f00b7c5c
John Vanek <62773153+JohnVanek@users.noreply.github.com>
2022-08-29 11:31:54 -0400

    course basic detailed course view

---
+++ app/src/main/java/com/example/android/mygreatuniversity/UI/CourseViewDetailed.java
@@ -0,0 +1,28 @@
+package com.example.android.mygreatuniversity.UI;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.example.android.mygreatuniversity.R;
+
+public class CourseViewDetailed extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course_view_detailed);
+
+        Toolbar myToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
+        //Define an actionbar reference for shorthand
+        ActionBar ab = getSupportActionBar();
+
+        //Set up the back is up when children are present.
+        assert ab != null;
+        ab.setDisplayHomeAsUpEnabled(true);   //show back button
+        ab.setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_24);
+    }
+}

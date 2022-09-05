918e90fd204ff947e03ea98c4d6e4caa1480519d
John Vanek <62773153+JohnVanek@users.noreply.github.com>
2022-09-05 09:08:28 -0400

    git token check 2

--- app/src/main/java/com/example/android/mygreatuniversity/UI/CourseViewDetailed.java
+++ app/src/main/java/com/example/android/mygreatuniversity/UI/CourseViewDetailed.java
@@ -51,7 +51,7 @@ public class CourseViewDetailed extends AppCompatActivity {
         Toolbar myToolbar = findViewById(R.id.toolbar);
         setSupportActionBar(myToolbar);
         //Define an actionbar reference for shorthand
-        //test
+        //test 2
         ActionBar actionBar = getSupportActionBar();
 
         //Set nav icon location as back if child

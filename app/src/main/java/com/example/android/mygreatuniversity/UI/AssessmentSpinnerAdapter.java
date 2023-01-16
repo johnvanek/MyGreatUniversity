c8373b5dc15c590cdc309c6c1661dfa7351d20be
John Vanek <jvanimal2@gmail.com>
2023-01-16 15:56:06 -0500

    add the AssessmentSpinnerAdapter class for using assessment values in a spinner

---
+++ app/src/main/java/com/example/android/mygreatuniversity/UI/AssessmentSpinnerAdapter.java
@@ -0,0 +1,64 @@
+package com.example.android.mygreatuniversity.UI;
+
+import android.content.Context;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.example.android.mygreatuniversity.Entity.Assessment;
+
+
+public class AssessmentSpinnerAdapter extends ArrayAdapter {
+    //FIELDS
+    private Context context;
+    private Assessment[] assessmentArray;
+    //CONSTRUCTOR
+    public AssessmentSpinnerAdapter(Context context, int resId, Assessment[] mentorArray) {
+        super(context, resId, mentorArray);
+        this.context = context;
+        this.assessmentArray = mentorArray;
+    }
+
+    @Override
+    public int getCount(){
+        return assessmentArray.length;
+    }
+
+    @Override
+    public Assessment getItem(int pos){
+        return assessmentArray[pos];
+    }
+
+    @Override
+    public long getItemId(int position){
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Create a text view
+        TextView assessmentTextView = (TextView) super.getView(position, convertView, parent);
+        // Set the text to the current position in the array and get the title
+        assessmentTextView.setText(assessmentArray[position].getTitle());
+        // Return the textView
+        return assessmentTextView;
+    }
+
+    // Normally is the same view, but you can customize it if you want
+    @Override
+    public View getDropDownView(int pos, View convertView,
+                                ViewGroup parent) {
+        TextView assessmentTextView = (TextView) super.getDropDownView(pos, convertView, parent);
+        //assessmentTextView.setTextColor(Color.BLACK);
+        //This should call the getName method on the assessment object
+        //But I im not sure these methods ever get called.
+        assessmentTextView.setText(assessmentArray[pos].getTitle());
+        return assessmentTextView;
+    }
+
+    public Assessment[] returnValuesAsArray (){
+        return assessmentArray;
+    }
+}

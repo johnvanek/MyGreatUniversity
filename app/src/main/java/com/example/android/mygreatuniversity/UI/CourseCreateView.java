06ee7490b9fa2464df5d36f26e10e1c924f392d5
John Vanek <jvanimal2@gmail.com>
2023-01-17 14:11:18 -0500

    add initial field declarations for CourseCreateView.java rework the create layout as to not affect repo data until creation occurs.

--- app/src/main/java/com/example/android/mygreatuniversity/UI/CourseCreateView.java
+++ app/src/main/java/com/example/android/mygreatuniversity/UI/CourseCreateView.java
@@ -1,4 +1,143 @@
 package com.example.android.mygreatuniversity.UI;
+// TODO this create view should be similar to how create term is handled
+//  Because of how repo works in reassigning these these recycler view values
+//  When the user is creating an items they shouldn't have the ability alter data in
+//  the repo until created.
 
-public class CourseCreateView {
+import static com.example.android.mygreatuniversity.Utils.Utils.courseStatusPosition;
+import static com.example.android.mygreatuniversity.Utils.Utils.hideKeyboard;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.android.mygreatuniversity.Database.Repo;
+import com.example.android.mygreatuniversity.Entity.Assessment;
+import com.example.android.mygreatuniversity.Entity.Course;
+import com.example.android.mygreatuniversity.Entity.Mentor;
+import com.example.android.mygreatuniversity.Entity.Term;
+import com.example.android.mygreatuniversity.R;
+import com.example.android.mygreatuniversity.Utils.StateManager;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
+
+public class CourseCreateView extends AppCompatActivity {
+    //TODO insert these fields but make sure to clean up the code referencing assessments
+    // And the recycler view might throw a runtime error. When it is attempted to be created.
+    //**************  START DECLARATIONS *********************
+    final Calendar CalenderStart = Calendar.getInstance();
+    final Calendar CalenderEnd = Calendar.getInstance();
+
+    boolean arrivedFromIntent = true;
+    //Field Declarations
+    EditText courseTitle;
+    EditText startText, endText, courseNoteEditText;
+    TextView mentorTextView, noteCardLabel, courseLabel;
+    Spinner courseStatus;
+    Spinner mentorSpinner;
+    Spinner assessmentSpinner;
+    AssessmentSpinnerAdapter assessmentSpinnerAdapter;
+    RecyclerView courseAssessmentRecyclerView;
+    MentorSpinnerAdapter mentorSpinnerAdapter;
+    CardView mentorCard,noteCard;
+    Button sendButton;
+
+    ConstraintLayout courseLayout,courseNoteLayout,generalLayout;
+    // Mentor fields
+    EditText mentorName, mentorPhone, mentorEmail;
+    //intent data references
+    String intentTitle, intentStartDate, intentEndDate, intentStatus;
+    //mentor intent strings
+    String mentorIntentName, mentorIntentPhone, mentorIntentEmail;
+
+    int intentTermId;
+    int intentCourseId, intentMentorId;
+    Repo repo = new Repo(getApplication());
+    List<Assessment>courseAssessments;
+    //Date References & Declarations
+    String format = "MM/dd/yy";
+    SimpleDateFormat dateFormat = new SimpleDateFormat(format, Locale.US);
+    DatePickerDialog.OnDateSetListener startDatePicker, endDatePicker;
+
+    //****************** END DECLARATIONS **************************************
+    //Start of the OnCreate Logic
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }

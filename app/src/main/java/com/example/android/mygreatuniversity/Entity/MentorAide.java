3545c64a5f89eaffc1d7a5b9adda1ee21dc8a321
John Vanek <jvanimal2@gmail.com>
2023-08-05 16:35:46 -0400

    add the MentorAide class

--- app/src/main/java/com/example/android/mygreatuniversity/Entity/MentorAide.java
+++ app/src/main/java/com/example/android/mygreatuniversity/Entity/MentorAide.java
@@ -1,6 +1,46 @@
 package com.example.android.mygreatuniversity.Entity;
 
-// Concrete classes
+
+import androidx.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.util.Arrays;
+
+@Entity(tableName = "mentorAides")
 public class MentorAide extends FacultyMisc {
-    // Additional fields and methods specific to MentorAide...
+    @PrimaryKey(autoGenerate = true)
+    private int mentorAideID;
+    private String[] subjects;
+    public MentorAide(String name, String email, String jobTitle, String[] availability, String[] subjects) {
+        super(name, email, jobTitle, availability);
+        this.subjects = subjects;
+    }
+
+    public int getMentorAideID() {
+        return mentorAideID;
+    }
+
+    public String[] getSubjects() {
+        return subjects;
+    }
+
+    public void setSubjects(String[] subjects) {
+        this.subjects = subjects;
+    }
+
+    // Overloaded method Contract - For PolyMorphism Requirement
+    // toString method Overriding is Runtime Polymorphism
+    // Overloading in static compile time Polymorphism
+    @NonNull
+    @Override
+    public String toString() {
+        return "Mentor-Aide{" +
+                "name='" + super.getName() + '\'' +
+                ", email='" + super.getEmail() + '\'' +
+                ", jobTitle='" + super.getJobTitle() + '\'' +
+                ", availability=" + Arrays.toString(super.getAvailability()) +
+                ", subjects='" + Arrays.toString(getSubjects()) + '\'' +
+                '}';
+    }
 }

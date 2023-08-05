b2d7a6163d4dc095581dd45570c8400560208ed2
John Vanek <jvanimal2@gmail.com>
2023-08-04 20:21:20 -0400

    add the user entity

---
+++ app/src/main/java/com/example/android/mygreatuniversity/Entity/User.java
@@ -0,0 +1,59 @@
+package com.example.android.mygreatuniversity.Entity;
+
+import androidx.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "users")
+public class User {
+    @PrimaryKey(autoGenerate = true)
+    private int userID;
+    private String name;
+    private String username;
+    private String password;
+
+    public User(String name, String username, String password) {
+        this.name = name;
+        this.username = username;
+        this.password = password;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return name;
+    }
+}
+

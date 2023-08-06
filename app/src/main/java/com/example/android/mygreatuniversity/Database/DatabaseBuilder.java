2a1e06072f1be6477ccec7723fa761a1a55b56a8
John Vanek <jvanimal2@gmail.com>
2023-08-05 20:18:53 -0400

    increment database

--- app/src/main/java/com/example/android/mygreatuniversity/Database/DatabaseBuilder.java
+++ app/src/main/java/com/example/android/mygreatuniversity/Database/DatabaseBuilder.java
@@ -36,7 +36,7 @@ import com.example.android.mygreatuniversity.Entity.User;
         Term.class,
         MentorAide.class,
         TechSupport.class},
-        version = 62, //Increment this after every database change
+        version = 63, //Increment this after every database change
         exportSchema = false)
 
 public abstract class DatabaseBuilder extends RoomDatabase {

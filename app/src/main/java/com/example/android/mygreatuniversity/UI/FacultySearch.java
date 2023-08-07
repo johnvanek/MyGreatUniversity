0cf7241c4378cd45906c9c94a4b69b92795ff03f
John Vanek <jvanimal2@gmail.com>
2023-08-06 22:08:44 -0400

    create and update the string array for the spinner FacultySearch

--- app/src/main/java/com/example/android/mygreatuniversity/UI/FacultySearch.java
+++ app/src/main/java/com/example/android/mygreatuniversity/UI/FacultySearch.java
@@ -1,10 +1,6 @@
 package com.example.android.mygreatuniversity.UI;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
